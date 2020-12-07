a3aa7bcbdd9050d44d316ddab25c5c8d426930fa
Fedor Batonogov <batonogov@outlook.com>
2020-12-07 14:45:49 +0300

    Мелкие изменения

--- r24.py
+++ r24.py
@@ -22,8 +22,8 @@ def start_mac():
 
 def start_win():
     # Запускаем браузер с необходимой страницей
-    run = '"start /max /b cmd /c ""C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk" -kiosk https://www.youtube.com/watch?v=8T9SFZDP60Q""'
-    # run = '"start /max /b cmd /c "FirefoxPortable/FirefoxPortable.exe -kiosk https://www.youtube.com/watch?v=8T9SFZDP60Q""'
+    # run = '"start /max /b cmd /c ""C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk" -kiosk https://www.youtube.com/watch?v=8T9SFZDP60Q""'
+    run = '"start /max /b cmd /c "FirefoxPortable/FirefoxPortable.exe -kiosk https://www.youtube.com/watch?v=8T9SFZDP60Q""'
     os.system(run)
     print('Запустил:', run)
 
@@ -76,7 +76,8 @@ if your_os == "darwin":
     time.sleep(10)
     click()
 elif your_os == "win32":
+    time.sleep(60)
     # Запускаем браузер
     start_win()
-    time.sleep(60)
+    time.sleep(90)
     click()
